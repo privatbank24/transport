19d1a6ffa96a1e77e463ef07175a8527b876282e
Viacheslav Savratsky <savratskywork@gmail.com>
2021-09-05 21:13:25 +0300

    small fixes

--- src/pages/MyTickets/index.tsx
+++ src/pages/MyTickets/index.tsx
@@ -1,10 +1,15 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import { Ticket } from "../../components/Ticket";
+import "./index.scss";
 
 export const MyTickets = () => {
   return (
-    <div>
-      <Ticket />
+    <div className="my-tickets">
+      <div>
+        <Ticket />
+      </div>
+      <Button>Отсканировать QR-код</Button>
     </div>
   );
 };
