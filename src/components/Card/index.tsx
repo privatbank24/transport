5e2729d5a0c5909ddcbefe717f04afe3272b6a8a
Viacheslav Savratsky <savratskywork@gmail.com>
2021-09-09 17:42:20 +0300

    created layout for card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Card = () => {
+  return (
+    <div className="card-component">
+      <img src="" alt="" />
+      <div className="card-component__content">
+        <div>
+          <h2>Карта универсальная</h2>
+          <h3>5109 **** **** 9234</h3>
+          <p>07/23</p>
+        </div>
+        <div>
+          <h1>234 UAH</h1>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -4,7 +4,23 @@ import React from "react";
 export const HomePage = () => {
   return (
     <Fade in>
-      <div>HomePage</div>
+      <div className="dashboard">
+        <div className="dashboard__main">
+          <div className="dashboard__main_top">
+            <div>
+              <img src="" alt="" />
+              <p>Кошелек</p>
+            </div>
+            <button>{">"}</button>
+          </div>
+          <div className="dashboard__main_cards">
+            {/* start */}
+
+            {/* end */}
+          </div>
+        </div>
+        <div className="dashboard__bottom"></div>
+      </div>
     </Fade>
   );
 };
